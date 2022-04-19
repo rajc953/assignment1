ed122694089b9c85d773bf89c6737d9ff8c76081
rajc953 <103478704+rajc953@users.noreply.github.com>
2022-04-19 13:40:11 +0530

    Add files via upload

---
+++ Q4_input.py
@@ -0,0 +1,8 @@
+m1 = int(input('enter the marks of first student: '))
+m2 = int(input('enter the marks of second student: '))
+m3 = int(input('enter the marks of third student: '))
+m4 = int(input('enter the marks of fouth student: '))
+m5 = int(input('enter the marks of fifth student: '))
+list = [m1,m2,m3,m4,m5]
+print(sorted(list))
+
---
+++ Q4_output.py
@@ -0,0 +1,18 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/HP/Dropbox/PC/Desktop/assignment1/assignment itc.py ====
+enter the marks of first student: 34
+enter the marks of second student: 43
+enter the marks of third student: 23
+enter the marks of fouth student: 45
+enter the marks of fifth student: 1
+[1, 23, 34, 43, 45]
+
+===== RESTART: C:/Users/HP/Dropbox/PC/Desktop/assignment1/assignment itc.py ====
+enter the marks of first student: 1
+enter the marks of second student: 4
+enter the marks of third student: 2
+enter the marks of fouth student: 20
+enter the marks of fifth student: 3
+[1, 2, 3, 4, 20]
